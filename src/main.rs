d6c0c4de38e5f0c063e153af7c142b634f557162
Eugene Kuzmenko <1evgenbetake@gmail.com>
2022-06-24 23:37:38 +0300

    Make hello world

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ const BACKGROUND_COLOR: Color = Color::rgb(0.0, 0.0, 0.0);
 
 fn main() {
     App::new()
+      .add_plugins(DefaultPlugins)
       .insert_resource(ClearColor(BACKGROUND_COLOR))
       .add_startup_system(setup_system)
       .add_system(bevy::input::system::exit_on_esc_system)
